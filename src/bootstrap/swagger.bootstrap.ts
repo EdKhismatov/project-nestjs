15efbc90c0c21d12a7f6394bc5d923a72a2fae25
EduardKhismatov <E.Khismatov@yandex.ru>
2026-01-20 16:32:38 +0600

    fix: swagger

---
+++ src/bootstrap/swagger.bootstrap.ts
@@ -0,0 +1,15 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const bootstrapSwagger = async (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Marketplace')
+    .setDescription('The marketplace API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+};
