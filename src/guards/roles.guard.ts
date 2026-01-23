690b8d36a1b25f0c92f839840692c14ce5ef3617
EduardKhismatov <E.Khismatov@yandex.ru>
2026-01-23 18:27:59 +0600

    fix: products

---
+++ src/guards/roles.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ForbiddenException } from '../exceptions';
+import { Roles } from './roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get(Roles, context.getHandler());
+    if (!requiredRoles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    const hasRole = requiredRoles.includes(user.role);
+    if (!hasRole) {
+      throw new ForbiddenException('У вас недостаточно прав для этой операции');
+    }
+    return true;
+  }
+}
--- src/module/products/dto/product.types.ts
+++ src/module/products/dto/product.types.ts
@@ -3,5 +3,13 @@ import { FastifyRequest } from 'fastify';
 export interface ProductRequest extends FastifyRequest {
   user: {
     id: string;
+    role: string;
+    email: string;
   };
+}
+
+export interface IActiveUser {
+  id: string;
+  role: string;
+  email: string;
 }
